dc9659f4ec4c156656667bd759dd4db1ab7f9f63
bagariariya16 <bagariaria16@gmail.com>
2025-02-12 10:44:14 +0530

    Java

---
+++ Day2/Java/FirstProject/src/com/mindsprint/project1/basics/Variables.java
@@ -0,0 +1,17 @@
+package com.mindsprint.project1.basics;
+
+public class Variables {
+    public static void main(String[] args) {
+        int num1 = 20;
+        float num2 = 34.56f;
+        double num3 = 79.95;
+        char c = 'A';
+        boolean check = 78>34;
+        System.out.println("Number1: " +num1);
+        System.out.println("Number2: " +num2);
+        System.out.println("Number3: " +num3);
+        System.out.println("character c: " +c);
+        System.out.println("check: " +check);
+
+    }
+}
